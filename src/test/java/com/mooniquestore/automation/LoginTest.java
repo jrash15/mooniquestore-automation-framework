d15f04525e8456ffd63a2696fc6667f957f9eb63
rashadjafarzadeh <jafarzaderashad@gmail.com>
2025-01-24 12:04:12 -0800

    Data Provider

--- src/test/java/com/mooniquestore/automation/LoginTest.java
+++ src/test/java/com/mooniquestore/automation/LoginTest.java
@@ -6,12 +6,21 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import com.mooniquestore.pom.implementation.LogInPageImpl;
 
+import java.util.Map;
+
 public class LoginTest extends MSBaseTest  {
 
     @Test(dataProvider = "CsvDataProvider", dataProviderClass =  CsvDataProvider.class)
-    public void positiveLogInTest() {
+    public void positiveLogInTest(Map<String, String> testData) {
         LogInPageImpl logInPageImpl = new LogInPageImpl(driver);
         UserAccountPageImpl userAccountPage = new UserAccountPageImpl(driver);
+        String expectedErrorMessage = testData.get("Email and/or password incorrect.");
+        String testNumber = testData.get("no");
+        String email = testData.get("email");
+        String password = testData.get("password");
+        String description = testData.get("description");
+
+        System.out.println("Test No # " + testNumber + "for " + description + "Where\nEmail: " + email + "\nPassword: " + password);
 
         //Step 1: Open Moonique Store Login page
         logInPageImpl.openLogInPage();
